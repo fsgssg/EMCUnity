db9d87454f8cbacd4509ad002c5b7a38c7f16769
J. Greg Williams <totallyGreg@gmail.com>
2018-03-15 13:40:13 -0500

    Added get_from_type <object_type> function and subcommand parser

--- EMC_cli.py
+++ EMC_cli.py
@@ -12,7 +12,6 @@ from EMCUnity import *
 module = sys.modules['__main__'].__file__
 log = logging.getLogger(module)
 
-
 def parse_command_line(argv):
     """Parse command line argument. See -h option
 
@@ -22,14 +21,14 @@ def parse_command_line(argv):
     parser = argparse.ArgumentParser(description=__doc__)
 
     # Optional Arguments
+    parser.add_argument('-e', '--host', dest='host', default="usdwest01emc",
+                        type=str, help='Hostname of EMC Unity device')
     parser.add_argument('-u', '--user', dest='user', type=str, default="restapi",
                         help='Username used for EMC Array login')
     parser.add_argument('-p', '--password', dest='password', action='store_true',
                         help='Prompt for Password')
     #parser.add_argument("--version", action="version",
     #                    version="%(prog)s {}".format(__version__))
-    parser.add_argument('-e', '--host', dest='host', default="usdwest01emc",
-                        type=str, help='Hostname of EMC Unity device')
     parser.add_argument("-v", "--verbose", dest="verbose_count",
                         action="count", default=0,
                         help="increases log verbosity for each occurence.")
@@ -38,28 +37,37 @@ def parse_command_line(argv):
                         help="redirect output to a file")
 
     # Sub-Commands
+    # Functions can be defined as their own subcommands
     subparsers = parser.add_subparsers(title='subcommands',
-                                       description='Common Commands',
+                                       description='valid subcommands',
                                        dest='subparser_name' )
 
-    def subcommand(func, parent=subparsers):
-        parser = parent.add_parser(func.__name__, help=func.__doc__)
+    def subcommand(func, aliases=[], parent=subparsers):
+        parser = parent.add_parser(func.__name__, aliases=aliases, help=func.__doc__)
         parser.set_defaults(func=func)
         return parser
 
-
     # def create_lun(self, lun_name, pool_id, size, lun_description=None):
     # Unity.create_lun.__code__.co_varnames
-    parser_create_lun = subcommand(Unity.create_lun)
-    parser_create_lun.add_argument('-o', '--pool', dest='pool', default="pool_1",
+    p_create_lun = subcommand(Unity.create_lun, aliases=['cl'])
+    p_create_lun.add_argument('-o', '--pool', dest='pool', default="pool_1",
                                    type=str, help='Name of Pool of str arg')
-    parser_create_lun.add_argument('-s', '--size', dest='size', default="p1099511627776",
+    p_create_lun.add_argument('-s', '--size', dest='size', default="p1099511627776",
                                    type=str, help='Name of Pool of str arg')
-    parser_create_lun.add_argument('lun_name', metavar='lun_name', type=str,
+    p_create_lun.add_argument('lun_name', metavar='lun_name', type=str,
                         help='Name of LUN to create in format: ldom-vol#')
 
-    parser_delete_lun = subcommand(Unity.delete_lun)
-    parser_delete_lun.add_argument('lun_id', metavar='lun_id', type=str,
+#    def get_from_type(self, url_path, object_type, payload = None):
+#        """
+#        Performs a request of all fields for a given object_type unless
+#        specific fields have been requested as part of the payload
+#        """
+    p_get_from_type = subcommand(Unity.get_from_type, aliases=['get'])
+    p_get_from_type.add_argument('object_type', metavar='object_type', type=str,
+                        help='the object type to return')
+
+    p_delete_lun = subcommand(Unity.delete_lun)
+    p_delete_lun.add_argument('lun_id', metavar='lun_id', type=str,
                                    help='Storage Resource lun_id')
 
 
@@ -71,7 +79,6 @@ def main():
     """Main program. Sets up logging and do some work."""
     log.info(sys.argv)
     args = parse_command_line(sys.argv)
-    print args
     levels = [logging.WARNING, logging.INFO, logging.DEBUG]
     level = levels[min(len(levels)-1,args.verbose_count)]  # capped to number of levels
     log.info("Arguments: {}".format(args))
